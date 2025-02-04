9d93d997d25222b20a8dfba1fb87b8181fa10afe
twotanawin <tanawin.twox@gmail.com>
2025-02-05 00:12:51 +0700

    config allow cors

--- BookAPI/src/main/java/com/tanawin/BookAPI/config/CorsConfig.java
+++ BookAPI/src/main/java/com/tanawin/BookAPI/config/CorsConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter; // ✅ Correct import
+import org.springframework.web.filter.CorsFilter; 
 
 import java.util.List;
 
